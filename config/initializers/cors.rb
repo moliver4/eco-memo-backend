9ed7d136c80dbdb1062b5f71d42e140c5db0b60c
moliver4 <tifftang22@gmail.com>
2020-02-11 16:27:58 -0800

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://eco-memo.herokuapp.com/', 'https://eco-memo.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
