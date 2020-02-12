29c16c3f1e03db9a3bab7dc30cd2c8e6937f308f
moliver4 <tifftang22@gmail.com>
2020-02-11 16:16:39 -0800

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://eco-memo.herokuapp.com/', 'https://eco-memo.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :delete]
+  end
+end
